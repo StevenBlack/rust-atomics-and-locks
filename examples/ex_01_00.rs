faa9d548a53c657b57e94a8457eeb83b8e88ef60
Steven Black <steveb@stevenblack.com>
2023-05-05 11:36:55 -0400

    Looping back to square 1, left this fallow for too long, let's review 🙂

---
+++ examples/ex_01_00.rs
@@ -0,0 +1,19 @@
+// Spawn two threads and print their ids. The main thread will wait for the
+// other threads to finish before exiting.
+use std::thread;
+
+fn main() {
+    let _t1 = thread::spawn(f);
+    let _t2 = thread::spawn(f);
+
+    println!("Hello from the main thread.");
+    // t1.join().unwrap();
+    // t2.join().unwrap();
+}
+
+fn f() {
+    println!("Hello from another thread!");
+
+    let id = thread::current().id();
+    println!("This is my thread id: {id:?}");
+}
